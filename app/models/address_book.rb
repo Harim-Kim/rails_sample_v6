a516685e01413b0547ff7d5adf91c076f68941e0
yongkal2@yonsei.ac.kr <yongkal2@yonsei.ac.kr>
2022-03-23 22:23:20 +0900

    ãvalidation add

--- app/models/address_book.rb
+++ app/models/address_book.rb
@@ -1,2 +1,8 @@
 class AddressBook < ApplicationRecord
+  validates :name,  :presence => true
+  validates :name,   format: {with: /^[가-힣]+$/, message: "한글 및 띄어쓰기만 입력 가능합니다(자음 혹은 모음만 입력 불가)"}
+  validates :age,   :presence => true
+  validates :age,   comparison: {less_than: 20}
+  validates :age,   numericality: {only_integer: true}
+
 end
